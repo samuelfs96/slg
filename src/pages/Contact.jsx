8b03c2f82e6465d2c392b2e0bd64217f8b184d39
Samuel <samuelfs96@gmail.com>
2022-11-03 20:00:25 -0400

    other pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,24 @@
 const Contact = () => {
-    return <h1>Contact</h1>;
+    return (
+      <div style={{paddingTop: '117px'}}>
+        <div className="container text-slg-gray mt-24 mb-24">
+          <h4 className="text-center text-2xl text-slg-blue font-bold m-2">Contacto</h4>
+          <p className="text-slg-gray m-2 text-center mt-12 mb-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Provident perspiciatis doloribus hic odio laboriosam. 
+            Ab id debitis magnam perspiciatis, culpa eveniet optio 
+            temporibus fugiat repellat qui voluptatem vitae dolorum 
+            reiciendis doloremque sunt unde. Minima, officiis harum. 
+            Quidem illum vero quas?
+            reiciendis doloremque sunt unde. Minima, officiis harum. 
+            Quidem illum vero quas?
+            reiciendis doloremque sunt unde. Minima, officiis harum. 
+            Quidem illum vero quas?
+            reiciendis doloremque sunt unde. Minima, officiis harum.
+          </p>
+        </div>
+      </div>
+    );
   };
   
   export default Contact;
--- src/pages/Sustentability.jsx
+++ src/pages/Sustentability.jsx
@@ -1,5 +1,24 @@
 const Sustentability = () => {
-    return <h1>Sustentability</h1>;
+    return (
+      <div style={{paddingTop: '117px'}}>
+        <div className="container text-slg-gray mt-24 mb-24">
+          <h4 className="text-center text-2xl text-slg-blue font-bold m-2">Sustentabilidad</h4>
+          <p className="text-slg-gray m-2 text-center mt-12 mb-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Provident perspiciatis doloribus hic odio laboriosam. 
+            Ab id debitis magnam perspiciatis, culpa eveniet optio 
+            temporibus fugiat repellat qui voluptatem vitae dolorum 
+            reiciendis doloremque sunt unde. Minima, officiis harum. 
+            Quidem illum vero quas?
+            reiciendis doloremque sunt unde. Minima, officiis harum. 
+            Quidem illum vero quas?
+            reiciendis doloremque sunt unde. Minima, officiis harum. 
+            Quidem illum vero quas?
+            reiciendis doloremque sunt unde. Minima, officiis harum.
+          </p>
+        </div>
+      </div>
+    );
   };
   
   export default Sustentability;
