0532e6889d62335e2b0fce5069a25928345a40de
Samuel Figueroa <32642706+samuelfs96@users.noreply.github.com>
2023-06-09 21:27:09 -0400

    Update static files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Rubik', 'sans-serif'],
+      'lato': ['Lato', 'sans-serif']
+    },
     extend: {
       container: {
         center: true,
@@ -17,13 +21,20 @@ module.exports = {
         }
       },
       colors: {
-        'slg-gray': '#6b7280',
-        'slg-blue': '#1e4b89'
+        'sgl-gray': '#6b7280',
+        'sgl-blue': '#1e4b89',
+        'sgl-blue-trans': 'rgb(30 75 137 / 65%)',
+        'sgl-black': '#282526'
       },
       backgroundImage: {
         'car1': "url('../src/img/car1.jpg')",
         'car2': "url('../src/img/car2.jpg')",
-        'car3': "url('../src/img/car3.jpg')"
+        'car3': "url('../src/img/car3.jpg')",
+        'bg1': "url('../src/img/bgimg-1.png')",
+        'bg2': "url('../src/img/bgimg-2.png')",
+        'bg3': "url('../src/img/bgimg-3.png')",
+        'bg4': "url('../src/img/bgimg-4.jpg')",
+        'bg5': "url('../src/img/bgimg-5.png')",
       }
     },
   },
